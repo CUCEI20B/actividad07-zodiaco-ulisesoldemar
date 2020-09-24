cd7726bddf6542af6438cf0a01a149bfb982d5aa
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-09-24 08:19:34 -0500

    prueba01

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("Hola Mundo")
+dia = int(input())
+mes = int(input())
+
+if mes == 1:
+    if dia <= 19:
+        print("capricornio")
+    else:
+        print("acuario")
+elif mes == 2:
+    if dia <= 18:
+        print("acuario")
+    else:
+        print("piscis")
+elif mes == 3:
+    if dia <= 20:
+        print("piscis")
+    else:
+        print("aries")
+elif mes == 4:
+    if dia <= 19:
+        print("aries")
+    else:
+        print("tauro")
+elif mes == 5:
+    if dia <= 20:
+        print("tauro")
+    else:
+        print("geminis")
+elif mes == 6:
+    if dia <= 20:
+        print("geminis")
+    else:
+        print("cancer")
+elif mes == 7:
+    if dia <= 22:
+        print("cancer")
+    else:
+        print("leo")
+elif mes == 8:
+    if dia <= 22:
+        print("leo")
+    else:
+        print("virgo")
+elif mes == 9:
+    if dia <= 22:
+        print("virgo")
+    else:
+        print("libra")
+elif mes == 10:
+    if dia <= 22:
+        print("libra")
+    else:
+        print("escorpio")
+elif mes == 11:
+    if dia <= 21:
+        print("escorpio")
+    else:
+        print("sagitario")
+elif mes == 12:
+    if dia <= 21:
+        print("sagitario")
+    else:
+        print("capricornio")
